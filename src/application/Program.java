6a47532093de0919160e5da40bf35750239b7624
curciogabriel <curcioog@gmail.com>
2025-02-14 16:18:50 -0300

    feat: add student class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,22 @@
 package application;
 
+import entities.Student;
+import java.util.Scanner;
+
 public class Program {
 
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Student student = new Student();
+
+        student.name = sc.nextLine();
+        student.trimester1 = sc.nextDouble();
+        student.trimester2 = sc.nextDouble();
+        student.trimester3 = sc.nextDouble();
+
+        student.approved();
+
+        sc.close();
+    }
 }
--- src/entities/Student.java
+++ src/entities/Student.java
@@ -2,5 +2,23 @@ package entities;
 
 public class Student {
 
+    public String name;
+    public double trimester1;
+    public double trimester2;
+    public double trimester3;
 
+    public void approved() {
+        double finalGrade = trimester1 + trimester2 + trimester3;
+        double missingPoints = 60 - finalGrade;
+
+        if (finalGrade > 60) {
+            System.out.printf("FINAL GRADE = %.2f%n", finalGrade);
+            System.out.println("PASS");
+        }
+        else {
+            System.out.printf("FINAL GRADE = %.2f%n", finalGrade);
+            System.out.println("FAILED");
+            System.out.printf("MISSING %.2f POINTS%n", missingPoints);
+        }
+    }
 }
